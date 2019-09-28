e550fa462b3b393b50a32624d6f9e93ffa485fb8
Alex Levine <a17levine@gmail.com>
2019-09-28 12:58:18 -0500

    Adding some error handling

--- app/controllers/tesla_controller.rb
+++ app/controllers/tesla_controller.rb
@@ -1,18 +1,22 @@
 class TeslaController < ApplicationController
   def start_hvac
-    # tesla_api = TeslaApi::Client.new(
-    #   email: ENV["tesla_email_address"], 
-    #   client_id: '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384', 
-    #   client_secret: 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3'
-    # )
-    # result = tesla_api.login!(ENV["tesla_password"])
-    # access_token = res['access_token']
-    # model_3 = tesla_api.vehicles.first # => <TeslaApi::Vehicle>
-    # model_3.wake_up
-    # model_3.auto_conditioning_start unless model_3.climate_state["is_auto_conditioning_on"]
-
     client = Slack::Web::Client.new
-    client.chat_postMessage(channel: '#alex-scripting-log', text: 'Hello World', as_user: true)
-    render json: {status: 'hvac started'}
+    begin
+      tesla_api = TeslaApi::Client.new(
+        email: ENV["tesla_email_address"], 
+        client_id: ENV["tesla_api_client_id"], 
+        client_secret: ENV["tesla_api_client_secret"]
+      )
+      tesla_api.login!(ENV["tesla_password"])
+      model_3 = tesla_api.vehicles.first
+      model_3.wake_up
+      model_3.auto_conditioning_start unless model_3.climate_state["is_auto_conditioning_on"]
+  
+      client.chat_postMessage(channel: '#alex-scripting-log', text: 'Tesla HVAC Started', as_user: true)
+      render json: { status: 'hvac started' }
+    rescue => e
+      client.chat_postMessage(channel: '#alex-scripting-log', text: "Tesla HVAC script failed: #{e.inspect} #{e.backtrace.join("\n")}", as_user: true)
+      render json: { status: 'hvac scriped failed' }
+    end
   end
 end
