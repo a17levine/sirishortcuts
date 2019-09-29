ed0ff288ec025134a91b137a7483438e273740a4
Alex Levine <a17levine@gmail.com>
2019-09-29 17:54:01 -0500

    Getting a retry cycle started for checking if the car is awake

--- app/controllers/tesla_controller.rb
+++ app/controllers/tesla_controller.rb
@@ -10,10 +10,21 @@ class TeslaController < ApplicationController
       tesla_api.login!(ENV["tesla_password"])
       model_3 = tesla_api.vehicles.first
       model_3.wake_up
-      model_3.auto_conditioning_start unless model_3.climate_state["is_auto_conditioning_on"]
-  
-      client.chat_postMessage(channel: '#alex-scripting-log', text: 'Tesla HVAC started', as_user: true)
-      render json: { status: 'hvac started' }
+
+      10.times {
+        puts "model3 state is #{model_3['state']}"
+        if model_3['state'] == 'online'
+          puts "turning on car"
+          model_3.auto_conditioning_start unless model_3.climate_state["is_auto_conditioning_on"]
+          client.chat_postMessage(channel: '#alex-scripting-log', text: 'Tesla HVAC started', as_user: true)
+          render json: { status: 'hvac started' } and return
+        else
+          puts "sleeping"
+          sleep 1
+        end
+      }
+      # If we're not in this loop, the car didn't wake up after several attempts
+      throw 'Car did not wake up after several attempts'
     rescue => e
       client.chat_postMessage(channel: '#alex-scripting-log', text: "Tesla HVAC script failed: #{e.inspect} #{e.backtrace.join("\n")}", as_user: true)
       render json: { status: 'hvac script failed' }
