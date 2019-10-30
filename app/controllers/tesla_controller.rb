3699731222b80fe93a9618789602b9d00b20f45b
Alex Levine <a17levine@gmail.com>
2019-10-29 23:27:39 -0500

    Updating for background job and IAM

--- app/controllers/tesla_controller.rb
+++ app/controllers/tesla_controller.rb
@@ -1,11 +1,14 @@
 class TeslaController < ApplicationController
   
   timeout 30
+  iam_policy "lambda"
   def start_hvac
     TeslaHvacJob.perform_later(:start_hvac)
     render json: { status: 'hvac started' } and return
   end
   
+  timeout 30
+  iam_policy "lambda"
   def stop_hvac
     TeslaHvacJob.perform_later(:stop_hvac)
     render json: { status: 'hvac stopped' } and return
